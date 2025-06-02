ea38b08ad102ee7ec7fad24c6581b55ab6c4eba1
Noam Stolero <noam@factify.com>
2025-06-02 13:59:29 +0300

    🐛 fix: update development environment configuration
    - Migrate to flake-parts for improved structure
    - Update direnv configuration with nix-direnv integration
    - Enhance shell welcome message with project info
    - Update starlette-admin dependency to stable version 0.15.1
    - Improve Python environment setup with better UV configuration

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
   inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
     nixpkgs-python = {
       url = "github:cachix/nixpkgs-python";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,54 +21,53 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    systems,
+  outputs = inputs @ {
+    flake-parts,
+    devenv-root,
     ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-      devenv-test = self.devShells.${system}.default.config.test;
-    });
-
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        py310 = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            ({config, ...}: {
-              # This is your devenv configuration
-              packages = with pkgs; [just pre-commit python3Packages.pre-commit-hooks];
-              languages.python = {
-                version = "3.10";
+  }:
+    flake-parts.lib.mkFlake {
+      inherit inputs;
+    } {
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          packages = with pkgs; [just pre-commit python3Packages.pre-commit-hooks];
+          languages.python = {
+            version = "3.10";
+            enable = true;
+            venv.enable = true;
+            uv = {
+              enable = true;
+              sync = {
                 enable = true;
-                uv = {
-                  enable = true;
-                  sync.enable = true;
-                };
+                allExtras = true;
               };
+            };
+          };
 
-              env = {
-                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc pkgs.zlib]; # NOTE: see https://github.com/cachix/devenv/issues/1264
-                UV_PYTHON_PREFERENCE = "only-system";
-                UV_LINK_MODE = "copy";
-                UV_PYTHON = "${config.languages.python.package}";
-              };
-              enterShell = ''
-                echo path to dll $NIX_LD
-                export VIRTUAL_ENV=$UV_PROJECT_ENVIRONMENT
-                source $VIRTUAL_ENV/bin/activate
-              '';
-            })
-          ];
+          env = {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc pkgs.zlib]; # NOTE: see https://github.com/cachix/devenv/issues/1264
+            UV_LINK_MODE = "copy";
+            UV_PYTHON = "${config.devenv.shells.default.languages.python.package}";
+          };
+
+          enterShell = ''
+            echo -e "\033[1;36m✨ \033[1;33mFactify DAL\033[1;36m ✨\033[0m"
+            echo -e "\033[0;32m→ Python \033[1;32m$(python --version | cut -d' ' -f2)\033[0m"
+            echo -e "\033[0;32m→ Run \033[1;32mjust\033[0m \033[0;32mfor available commands\033[0m"
+          '';
         };
-      });
-  };
+      };
+    };
 }
